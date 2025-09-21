45969e87db155905c6cb266e698838d4e00234e8
Shashank048 <136824394+Shashank048@users.noreply.github.com>
2025-09-21 18:38:18 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -20,7 +20,7 @@ app.use(express.json());
 app.use('/auth', authRoutes);
 
 const PORT = 2000;
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.MONGO_URI)
   .then(() => {
     console.log('Mongo connected');
     app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
